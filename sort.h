eb83e029fb843ec88c983f3a1c44e3031bd3457f
Bryan Yep <yepsan95@gmail.com>
2023-05-22 14:36:32 -0700

    fix: updated header file

--- sort.h
+++ sort.h
@@ -25,4 +25,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+void swap_nodes(listint_t *node_a, listint_t *node_b);
+
 #endif /*SORT_H*/
