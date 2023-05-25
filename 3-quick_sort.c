a9e79465596a905a3fb19380b593677d5a8fde7c
Bryan Yep <yepsan95@gmail.com>
2023-05-25 15:22:43 -0700

    fix: fixed print format

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,31 +10,79 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	int pivot, tmp;
-	size_t i, j;
-
 	if (array == NULL || size <= 1)
 		return;
+	quick_recursion(array, 0, size - 1, size);
+}
 
-	pivot = array[size - 1];
-	i = -1;
-	for (j = i + 1; j <= size - 1; j++)
+/**
+ * quick_recursion - sorts an array of integers in ascending
+ *                   order using the Quick sort algorithm
+ * @array: array of integers to be sorted
+ * @first: index of the first element in the partition
+ * @last: index of the last element in the partition
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void quick_recursion(int *array, int first, int last, size_t size)
+{
+	int pivot;
+
+	if (array == NULL || first >= last)
+		return;
+
+	pivot = partition(array, first, last, size);
+
+	quick_recursion(array, first, pivot - 1, size);
+	quick_recursion(array, pivot + 1, size - 1, size);
+}
+/**
+ * partition - performs a Quick sort in a partition of an array of integers
+ * @array: array of integers to be sorted
+ * @first: index of the first element in the partition
+ * @last: index of the last element in the partition
+ * @size: total size of the original array
+ *
+ * Return: void
+ */
+int partition(int *array, int first, int last, size_t size)
+{
+	int pivot, i, j;
+
+	pivot = array[last];
+	i = first - 1;
+	for (j = first; j < last; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
-			tmp = array[i];
-			array[i] = array[j];
-			array[j] = tmp;
-			print_array(array, size);
+			swap_int(&array[i], &array[j]);
+			if (i != j)
+				print_array(array, size);
 		}
 	}
-	tmp = array[i + 1];
-	array[i + 1] = array[size - 1];
-	array[size - 1] = tmp;
-	pivot = array[i + 1];
-	print_array(array, size);
+	i++;
+	swap_int(&array[i], &array[last]);
+	pivot = array[i];
+	if (i != j)
+		print_array(array, size);
 
-	quick_sort(array, i + 1);
-	quick_sort(array + i + 1, size - 1 - i);
+	return (i);
+}
+
+/**
+ * swap_int - swaps to integers from an arrray
+ * @a: pointer to first integer
+ * @b: pointer to second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
