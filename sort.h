14645359d26ff3334b89f14dd0b64df235c16bbc
Bryan Yep <yepsan95@gmail.com>
2023-05-24 20:56:24 -0700

    fix: updated header file

--- sort.h
+++ sort.h
@@ -24,7 +24,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-quick_recursion(int *array, size_t size);
+void quick_recursion(int *array, size_t size);
 
 void swap_node(listint_t *node_a, listint_t *node_b);
 listint_t *create_listint(const int *array, size_t size);
